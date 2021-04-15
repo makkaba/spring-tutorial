f949e6b4485f5296a7711e1e3a41d3a6560197e6
Jeff Chung <drifterz303@gmail.com>
2021-04-16 00:51:32 +0900

    repository 추가, 테스트코드 추가
    테스트 추가 cmd shift t
    리턴된 변수 받기 alt cmd v

---
+++ src/test/java/com/example/demo/MemberRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Transactional
+    @Test
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member foundMember = memberRepository.find(saveId);
+
+        //then
+        Assertions.assertThat(foundMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(foundMember.getUsername()).isEqualTo(member.getUsername());
+    }
+}
